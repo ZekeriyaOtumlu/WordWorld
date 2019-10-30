5e9967f18469c71ee5775baa3b31cd58d33d09d1
Zack <zotumlu78@gmail.com>
2019-10-29 20:22:39 -0400

    changed url

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 // $("#search-content").hide();
-var APIKey = "ae2b16a8-cb46-4eb5-9ede-3215ef2cb615";
+var APIKeyDef = "ae2b16a8-cb46-4eb5-9ede-3215ef2cb615";
 var userInput;
 
 function clickedSearchBtn(event){
@@ -15,7 +15,7 @@ function clickedSearchBtn(event){
     // $("#word-text").append(userInput);  
     console.log(userInput);
     
-    var queryURLDictionary = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"+ userInput+ "?key=" +APIKey;
+    var queryURLDictionary = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"+ userInput+ "?key=" +APIKeyDef;
     $.ajax({
         url: queryURLDictionary,
         method:"GET"
@@ -23,6 +23,15 @@ function clickedSearchBtn(event){
       console.log(queryURLDictionary);
       console.log(response);
     });
+    var APIKeyThesaurus = "2e33d79f-c228-40c0-9dfb-171729314d2a";
+    var queryURLThesaurus = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/"+userInput+ "?key=" +APIKeyThesaurus;
+    $.ajax({
+      url: queryURLThesaurus,
+      method:"GET"
+  }).then(function(response){
+    console.log(queryURLThesaurus);
+    console.log(response);
+  });
  for (var i =0; i<10; i++){
 
 
